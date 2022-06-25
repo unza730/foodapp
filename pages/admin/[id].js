d2b962b3860acd788c449e2e449a13eeb5675c77
unza730122 <sarisha505@gmail.com>
2022-06-25 21:53:20 +0500

    31th-commit

--- pages/admin/[id].js
+++ pages/admin/[id].js
@@ -43,7 +43,7 @@ const Add = ({ pizza }) => {
         extra,
         extraOptions,
       };
-      await axios.put(`${baseUrl}/api/products/${pizza._id}`, newProduct);
+      await axios.put(`/api/products/${pizza._id}`, newProduct);
       // router.push(`${baseUrl}`);
       console.log("SuccessFully Added!");
       router.push('/');
@@ -194,7 +194,7 @@ const Add = ({ pizza }) => {
   );
 };
 export const getServerSideProps = async ({ params }) => {
-  const res = await axios.get(`${baseUrl}/api/products/${params.id}`);
+  const res = await axios.get(`/api/products/${params.id}`);
   return {
     props: {
       pizza: res.data,
