b5784b88a0daf2ed39674bd4e1a51de16fe371dd
unza730122 <sarisha505@gmail.com>
2022-06-27 09:26:22 +0500

    45th-commit

--- pages/admin/[id].js
+++ pages/admin/[id].js
@@ -72,6 +72,7 @@ const Add = ({ pizza }) => {
               type="file"
               id="full-name"
               name="file"
+              value={pizza.img}
               // value={file[0]}
               className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               onChange={(e) => setFile(e.target.files[0])}
